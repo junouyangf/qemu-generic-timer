7f45abaed6f543b76911d45e7af06450e2bdfa23
Angel00021 <101666898+Angel00021@users.noreply.github.com>
2023-05-02 18:33:46 +0200

    Delete P3_SysTimer_0 directory

--- P3_SysTimer_0/debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-arm-none-eabi-gdb kernel.elf -ex "target remote localhost:1234"
-
-
--- P3_SysTimer_0/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-~/qemu-master/build/qemu-system-arm  -M raspi0 -kernel kernel.elf -s -S -nographic -qtest unix:/tmp/tmp-gpio.sock 
-	
