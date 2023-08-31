2258debc9365f952f76db350aa48c5ea7118156c
Angel00021 <101666898+Angel00021@users.noreply.github.com>
2023-08-31 20:41:29 +0200

    Update dependencias.sh

--- GUI/dependencias.sh
+++ GUI/dependencias.sh
@@ -6,15 +6,11 @@ sudo apt install python3 -y
 
 # Instala Tkinter 
 sudo apt install python3-tk -y
-
 # Instala ImageTk 
 sudo apt-get install python3-pil.imagetk
 sudo apt-get -y install socat
-
-sudo apt install python3-dev -y
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py
-sudo pip3 install pexpect
-sudo pip install --upgrade psutil
+python3 -m pip install psutil
+#GDB
+sudo apt-get install gdb-multiarch
 
 echo "Installation complete."
