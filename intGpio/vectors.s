ccde0b424aab0c6be0b1e25c3178c8d5a5d114fe
Angel00021 <101666898+Angel00021@users.noreply.github.com>
2023-09-03 12:49:41 +0200

    Update vectors.s

--- intGpio/vectors.s
+++ intGpio/vectors.s
@@ -1,25 +1,18 @@
-//EGGMAN
-// To keep this in the first portion of the binary.
-.section ".text.boot"
- 
-// Make _start global.
+.text
 .globl _start
  
 _start:
-    // in QEMU all of 4 ARM CPUs are started simultaniously
-    // by default. I don't know if this is the real hw behaviour,
-    // but here I jump to halt if CPU ID (stored in MPIDR
-    // register, first 2 bits) is not 0
+
     mrs   x1, mpidr_el1
     and   x1, x1, #3
     cmp   x1, #0
     bne   hang 
 
-    // address for stack pointer
+    // direccion SP
     ldr   x1, =_start
 
-    // drop to EL2
-    mov   x2, #0x5b1    // RW=1, HCE=1, SMD=1, RES=1, NS=1
+    // bajar a EL2
+    mov   x2, #0x401    // RW=1, NS=1
     msr   scr_el3, x2
     mov   x2, #0x3c9    // D=1, A=1, I=1, F=1 M=EL2h
     msr   spsr_el3, x2
@@ -28,17 +21,17 @@ _start:
     eret
 
 start_el2:
-    // set sp in EL1
+    // SP de EL1
     msr   sp_el1, x1
-    // enable AArch64 in EL1
+  
     mov   x0, #(1 << 31)      // AArch64
-    orr   x0, x0, #(1 << 1)   // SWIO hardwired on Pi3
+    
     msr   hcr_el2, x0
-    mrs   x0, hcr_el2
-    // set vector address in EL1.
+
+    // ponemos el vector de excepiciones a EL1
     ldr x0, =vector
     msr vbar_el1, x0 
-    // change execution level to EL1
+
     mov   x2, #0x3c4         // D=1, A=1, I=1, F=1 M=EL1t
     msr   spsr_el2, x2
     adr   x2, start_el1
@@ -47,16 +40,9 @@ start_el2:
 
 start_el1:
     // set sp
-    mov   sp, x1
-    // Clear bss.
-    ldr   x1, =__bss_start
-    ldr   w2, =__bss_size
-1:  cbz   w2, 2f
-    str   xzr, [x1], #8
-    sub   w2, w2, #1
-    cbnz  w2, 1b
+    mov   sp, #0x08000000
 
-2:  bl main
+    bl main
 
 hang:
     wfi
@@ -70,6 +56,16 @@ enable_irq:
 .globl disable_irq
 disable_irq:
     msr   daifset, #2
+    ret
+    
+.globl PUT32
+PUT32:
+    str x1,[x0]
+    ret
+
+.globl GET32
+GET32:
+    ldr x0,[x0]
     ret
 
 irq:
@@ -111,7 +107,7 @@ vector:
 .balign 128
     b hang
 .balign 128
-    b irq
+    b hang //
 .balign 128
     b hang
 .balign 128
@@ -119,7 +115,7 @@ vector:
 .balign 128
     b hang
 .balign 128
-    b irq
+    b hang //
 .balign 128
     b hang
 .balign 128
@@ -127,7 +123,7 @@ vector:
 .balign 128
     b hang
 .balign 128
-    b irq
+    b hang //
 .balign 128
     b hang
 .balign 128
